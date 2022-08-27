ad7c9a2d181e1413bf321c35f0ff39a1cf44c9dd
Vesnushk1n <myniki998@gmail.com>
2022-08-27 12:58:04 +0300

    дз

--- bm5_ch2.py
+++ bm5_ch2.py
@@ -1,13 +1,13 @@
 import json
-class Save:
-	def save(self, title, text, author):
-		result = json.dumps({'title': title, 'text': text, 'author': author})
-		path = f'{author}.json'
-		open(path, 'w').write(result)
-		return f"Сохранено в {path}"
+class Citatka:
+	def __init__(self, title, text, author):
+		self.title = title
+		self.text = text
+		self.author = author
+		open(f'Phrase.json', 'w').write(json.dumps(self.__dict__))
 while(True):
-	cit = Save()
 	title = input('Title: ')
 	text = input('Text: ')
 	author = input('Author: ')
-	print(cit.save(title, text, author))
+	Citatka(title, text, author)
+	print('Done!')
